b59bc8acf65e7a1084572d0c9ab08bf1a2a50afe
Namratatiwari30 <namratatiwari2005@gmail.com>
2026-07-24 13:30:46 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("student_scores.csv")
+
+X = df[["Hours"]]
+y = df["Score"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Training Data:")
+print(X_train)
+
+print("\nTesting Data:")
+print(X_test)
