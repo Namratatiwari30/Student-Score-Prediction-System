137dead3bf0dfb25127c19acaa9c7e49313d4e53
Namratatiwari30 <namratatiwari2005@gmail.com>
2026-07-24 13:14:43 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,18 @@
+name = "Namrata"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+a = 10
+b = 5
+
+print("Addition:", a + b)
+
+for i in range(1, 6):
+    print(i)
+
+def square(x):
+    return x * x
+
+print("Square =", square(5))
