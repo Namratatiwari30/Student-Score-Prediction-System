49a74e458ffe9755d665c8fe4e99be57d51dd3e7
Namratatiwari30 <namratatiwari2005@gmail.com>
2026-07-24 13:31:46 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+df = pd.read_csv("student_scores.csv")
+
+X = df[["Hours"]]
+y = df["Score"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("MAE =", mean_absolute_error(y_test, y_pred))
+print("MSE =", mean_squared_error(y_test, y_pred))
+print("R2 Score =", r2_score(y_test, y_pred))
